c919e432561e017bedabbcb9d03a9530138c6e27
stranger1618 <stranger1618@mail.ru>
2018-10-28 00:29:35 +0300

    добавили 2-ю строку

--- src/main/java/BaseClass1.java
+++ src/main/java/BaseClass1.java
@@ -1,7 +1,7 @@
 public class BaseClass1 {
     public static void main(String[] args) {
         System.out.println("Hi!  %)");
-        System.out.println("Add string");
+        System.out.println("Add string1");
 
     }
 }
